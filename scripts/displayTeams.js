d2b8c9042cc9d829e723a1bf7932d14fc27c0e39
Berezovsky Alexey <lazfnol@gmail.com>
2024-06-14 02:58:54 +0300

    modify teams rendering

--- scripts/displayTeams.js
+++ scripts/displayTeams.js
@@ -10,11 +10,11 @@ function displayTeams() {
       teamElement.innerHTML = `
       <div class="team">  
         <h3>&nbsp;&nbsp;&nbsp;${team.pointsCounter}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${team.name}</h3>
-          <select class="answers">
-          <option class="options">1</option>
-          <option class="options">2</option>
-          <option class="options">3</option>
-          <option class="options">4</option>
+          <select class="answers dynamic-dropdown" id="${team.name}">
+          <option value="1" class="options">1</option>
+          <option value="2" class="options">2</option>
+          <option value="3" class="options">3</option>
+          <option value="4" class="options">4</option>
         </select>
       </div>
       `;
@@ -25,5 +25,4 @@ function displayTeams() {
   }
 }
 
-// Вызываем функцию для отображения команд при загрузке страницы
 document.addEventListener('DOMContentLoaded', displayTeams);
