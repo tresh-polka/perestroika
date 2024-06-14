49a8698664067ac0d10f9d64a49d3861292d7679
Berezovsky Alexey <lazfnol@gmail.com>
2024-06-14 03:02:37 +0300

    changing teams saving in localStorage

--- scripts/teamsDivision.js
+++ scripts/teamsDivision.js
@@ -7,20 +7,36 @@ function saveTeamsToLocalStorage() {
   for (let i = 1; i <= 4; i += 1) {
     const teamName = document.getElementById(`team${i}`).value;
     const teamOption = document.getElementById(`option${i}`).value;
-    
+    switch (teamOption) {
+      case '1':
+        mainList = listForDogmatic1;
+        break;
+      case '2':
+        mainList = listForReformers2;
+        break;
+      case '3':
+        mainList = listForUndecided3;
+        break;
+    }
     if (teamName === '') {
       continue;
     } else {
-    teams.push({ name: teamName, option: teamOption, pointsCounter: 0 });
+    teams.push({ name: teamName, option: teamOption, pointsCounter: 0, answers: mainList });
     }
   }
 
   localStorage.setItem('teams', JSON.stringify(teams));
 }
 
-// Добавляем обработчик клика на кнопку "дальше ⮕"
+// Добавляем обработчик клика на кнопку
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('nextButton').addEventListener('click', function(event) {
     saveTeamsToLocalStorage();
+    const redirectUrl = this.getAttribute('data-url');
+        if (redirectUrl) {
+            window.location.href = redirectUrl;
+        } else {
+            console.log('Redirect URL not specified');
+        }
   });
 });
